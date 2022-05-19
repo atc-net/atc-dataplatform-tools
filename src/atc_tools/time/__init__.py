73bf8abfe6cb6430530472bda03015ace3f57490
Simon Heisterkamp <she@delegate.dk>
2022-05-19 09:59:47 +0200

    simple helper class for time-based tests

---
+++ src/atc_tools/time/__init__.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timezone, date
+
+from .TimeSequence import TimeSequence  # noqa: F401
+
+
+def dt_utc(*args, **kwargs) -> datetime:
+    if args or kwargs:
+        return datetime(*args, tzinfo=timezone.utc, **kwargs)
+    else:
+        return datetime.now(timezone.utc)
+
+
+dt_iso = datetime.fromisoformat
+d_iso = date.fromisoformat
