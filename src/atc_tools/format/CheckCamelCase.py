eb2f5a525a75eabfb78b161097e460659f39f022
Lau Johansson <ljo@delegate.dk>
2022-05-20 10:11:29 +0200

    test of camelcase checker

--- src/atc_tools/format/CheckCamelCase.py
+++ src/atc_tools/format/CheckCamelCase.py
@@ -2,31 +2,35 @@ from typing import List
 from pyspark.sql import DataFrame
 import re
 
-def DfIsCamel(df: DataFrame, cols_to_check: List[str] = None, print_result: bool = False):
-    """
-    :param df: A pyspark dataframe
-    :param cols_to_check: Optional, a subset of the columns in the dataframe to check.
-    :return: True if all columns are camelcased
+
+def DfIsCamel(
+    df: DataFrame, cols_to_check: List[str] = None, print_result: bool = False
+):
     """
 
+    :param df: A pyspark dataframe
+    :param cols_to_check: Optional, a subset of the columns in the dataframe to check.
+    :param print_result: If True, then the cols which are not camelCased are printed
+    :return: True if all columns are camelcased
+    """
 
     if cols_to_check is None:
         cols_to_check = df.columns
     else:
-        if not all(col in cols_to_check for col in df.columns):
+        if not all(col in df.columns for col in cols_to_check):
             raise ValueError("Some of the columns to check is not in the dataframe.")
 
-
-    results = []
+    results_col = []
+    check_passed = True
     for col in cols_to_check:
-        # If camelcased (regex 1) and dont have two uppercases (regex  then True
-        isCamelCased = bool(re.match("[a-z]+([A-Z][a-zA-Z]*)?", col))
-        hasTwoUpper =  bool(re.match(".*[A-Z][A-Z]", col))
-        results.append(isCamelCased and not hasTwoUpper)
+        is_camel_cased = bool(re.match("[a-z]+([A-Z][a-zA-Z]*)?", col))
+        has_two_upper = bool(re.match(".*[A-Z][A-Z]", col))
+        if not is_camel_cased or has_two_upper:
+            check_passed = False
+            results_col.append(col)
 
     # Todo : print results
     if print_result:
-        cols_to_check[results]
+        print(f"The following columns is not camelCased: {results_col}")
 
-    return all(results)
-
+    return check_passed
---
+++ tests/unit/test_CheckCamel.py
@@ -0,0 +1,157 @@
+import unittest
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from atc.spark import Spark
+
+from src.atc_tools.format.CheckCamelCase import DfIsCamel
+
+
+class CamelCaseTest(unittest.TestCase):
+    def test_01_pass(self):
+
+        data1 = [
+            (None,),
+        ]
+
+        schema1 = StructType(
+            [
+                StructField("camelCased", StringType(), True),
+            ]
+        )
+
+        df = Spark.get().createDataFrame(data=data1, schema=schema1)
+
+        self.assertTrue(DfIsCamel(df))
+
+    def test_02_pass(self):
+
+        data2 = [
+            (
+                None,
+                None,
+            ),
+        ]
+
+        schema2 = StructType(
+            [
+                StructField("camelCased", StringType(), True),
+                StructField("camelCasedAnother", StringType(), True),
+            ]
+        )
+
+        df = Spark.get().createDataFrame(data=data2, schema=schema2)
+
+        self.assertTrue(DfIsCamel(df))
+
+    def test_03_subset_pass(self):
+
+        data3 = [
+            (
+                None,
+                None,
+            ),
+        ]
+
+        schema3 = StructType(
+            [
+                StructField("camelCased", StringType(), True),
+                StructField("NotcamelCased", StringType(), True),
+            ]
+        )
+
+        df = Spark.get().createDataFrame(data=data3, schema=schema3)
+
+        self.assertTrue(DfIsCamel(df, ["camelCased"]))
+
+    def test_04_no_pass(self):
+
+        data4 = [
+            (
+                None,
+                None,
+            ),
+        ]
+
+        schema4 = StructType(
+            [
+                StructField("camelCased", StringType(), True),
+                StructField("NotcamelCased", StringType(), True),
+            ]
+        )
+
+        df = Spark.get().createDataFrame(data=data4, schema=schema4)
+
+        self.assertFalse(
+            DfIsCamel(
+                df,
+            )
+        )
+
+    def test_05_no_pass(self):
+
+        data5 = [
+            (
+                None,
+                None,
+            ),
+        ]
+
+        schema5 = StructType(
+            [
+                StructField("NotcamelCased1", StringType(), True),
+                StructField("NotcamelCased2", StringType(), True),
+            ]
+        )
+
+        df = Spark.get().createDataFrame(data=data5, schema=schema5)
+
+        self.assertFalse(
+            DfIsCamel(
+                df,
+            )
+        )
+
+    def test_07_no_pass(self):
+
+        data7 = [
+            (
+                None,
+                None,
+            ),
+        ]
+
+        schema7 = StructType(
+            [
+                StructField("camelCased", StringType(), True),
+                StructField("notCamelCAsed", StringType(), True),
+            ]
+        )
+
+        df = Spark.get().createDataFrame(data=data7, schema=schema7)
+
+        self.assertFalse(DfIsCamel(df, ["notCamelCAsed"]))
+
+    def test_08_valueerror(self):
+        data8 = [
+            (
+                None,
+                None,
+            ),
+        ]
+
+        schema8 = StructType(
+            [
+                StructField("colA", StringType(), True),
+                StructField("ColB", StringType(), True),
+            ]
+        )
+
+        df = Spark.get().createDataFrame(data=data8, schema=schema8)
+
+        with self.assertRaises(ValueError) as cm:
+            DfIsCamel(df, ["colC"])
+        the_exception = cm.exception
+
+        self.assertEqual(
+            str(the_exception),
+            str(ValueError("Some of the columns to check is not in the dataframe.")),
+        )
